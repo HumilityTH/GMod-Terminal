2359924e70f21147c8d817b9e0f10462b19edff0
Lapin <contact@extrem-team.com>
2015-01-03 19:03:56 +0100

    Windows 8 update (unfinished)
    The polydraw shit wont work

--- lua/autorun/terminal.lua
+++ lua/autorun/terminal.lua
@@ -23,8 +23,10 @@ Term.Colors = {
 				textarea = Color(0, 0, 0),
 				consoletext = Color(200, 200, 200)
 			}
+ 
 
-
+			
+Term.FullSize = false
 
 
 --Global functions
@@ -44,6 +46,54 @@ function Term.file_exists(f)
 	if file.Exists(f, "BASE_PATH") then return true end --Check if the file exists.
 end
 
+if CLIENT then
+
+local function MakeCirclePoly( _x, _y, _r, _points ) -- thx acecool // _if you fucking _dmca me im going to fucking _eat your family
+	local _u = ( _x + _r * 1 ) - _x;
+	local _v = ( _y + _r * 1 ) - _y;
+	local _slices = ( 2 * math.pi ) / _points;
+	local _poly = { };
+	for i = 0, _points - 1 do local _angle = ( _slices * i ) % _points;
+		local x = _x + _r * math.cos( _angle );
+		local y = _y + _r * math.sin( _angle );
+	table.insert( _poly, { x = x, y = y, u = _u, v = _v } ) end return _poly;
+end
+
+local circle1 = MakeCirclePoly(443, 263, 10, 100)
+local circle2 = MakeCirclePoly(443, 263, 8.9, 100)
+
+
+
+-- i tried
+local arrow = 
+{
+	{ x = 438, y = 264 },
+	{ x = 438, y = 259 },
+--	{ x = 342, y = 273 }, -- mid up -- fuck gmod poly system
+	{ x = 448, y = 259 },
+	{ x = 448	, y = 264 },
+	{ x = 443, y = 269 }, -- mid down
+}
+
+local arrow2 =  -- lets eat a part of the arrow
+{
+	{ x = 437, y = 258 },
+	{ x = 449, y = 258 },
+	{ x = 443, y = 263.5 }, -- I FUCKIING HATE VECTORS
+}
+
+surface.CreateFont( "WindowsPopUpTitle", {
+	font = "Segoe UI",
+	size = 24,
+} )
+
+surface.CreateFont( "WindowsPopUpTitle2", {
+	font = "Segoe UI",
+	size = 20,
+} )
+
+
+end
 
 --------------------------------
 ---- Including the commands ----
@@ -75,17 +125,33 @@ function Term.Menu() --Function for drawing the menu
 	frame:ShowCloseButton(false) --We do not need a close button as I made a custom one below. (See frameclose function)
 	frame:MakePopup() --Make the frame resease the mouse.
 	function frame:Paint(w,h) --This function is so we can draw on the frame.
-		draw.RoundedBox(0, 0, 0, w, h, Term.Colors['background']) --Changing the background colour.
-		draw.RoundedBox(0, 2, 25, w - 4, h - 27, Term.Colors['textarea']) --Draw a area for text box.
-		draw.SimpleText("Terminal", "terminaltitle", 5, 5, Term.Colors['titlefont']) --Draw the title.
+	
+	
+		
+	--	draw.RoundedBox(0, 0, 0, w, h, Term.Colors['background']) --Changing the background colour.
+	--	draw.RoundedBox(0, 2, 25, w - 4, h - 27, Term.Colors['textarea']) --Draw a area for text box.
+	--	draw.SimpleText("Terminal", "terminaltitle", 5, 5, Term.Colors['titlefont']) --Draw the title.
+
+	--surface.SetDrawColor(Color(64, 134, 195, 250)) -- Background outline
+	--surface.DrawRect(0, 0, 370, 250)
+	draw.RoundedBox(0, 0, 0, w, h, Color(64, 134, 195))
+	
+	--surface.SetDrawColor(Color(84, 175, 255)) -- Background
+	--surface.DrawRect(-21, 50, 368, 248)
+	draw.RoundedBox(0, 1, 1, w-2, h-2, Color(84, 175, 255))
+
+	draw.SimpleText("C:\\WINDOWS\\system32\\cmd.exe", "WindowsPopUpTitle", textarea:GetSize()/2, 15 , Color(0,0,0,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+
+
+		
 	end
 
 	textarea = vgui.Create("DTextEntry", frame) --Create the text area
-	textarea:SetSize(frame:GetWide() - 4, frame:GetTall() - 47) --Set the size
+	textarea:SetSize(frame:GetWide() - 18, frame:GetTall() - 60) --Set the size
 	textarea:SetMultiline(true) --Multiple lines
-	textarea:SetPos(2, 25) --Set pos
-	textarea:SetEditable(true)  --I would prefer to have this false, but it is the only way to make the text area
-								--highlightable / make the scroll bar work. If you know another way let me know.
+	textarea:SetPos(9, 33) --Set pos
+	textarea:SetMouseInputEnabled( true )
+	textarea:SetKeyboardInputEnabled( false )
 
 	if Term.Text == "" then --If the text in the terminal is not already set
 		textarea:SetText(Term.DefaultText) --Put the default text in
@@ -95,7 +161,9 @@ function Term.Menu() --Function for drawing the menu
 	textarea:SetVerticalScrollbarEnabled(true) --So we can scroll.
 	textarea:SetFont("terminalfont") --Setting the font to our terminal font.
 	function textarea:Paint(w, h) --Lets theme the textarea
-		draw.RoundedBox(0, 0, 0, w, h, Term.Colors['textarea']) --This is the background colour.
+		draw.RoundedBox(0, 0, 0, w-18, h-15, Color(64, 134, 195))
+		draw.RoundedBox(0, w-18, 0, 17, h, Color(240, 240, 240))
+		draw.RoundedBox(0, 1, 1, w-20, h-17, Term.Colors['textarea']) --This is the background colour.
 		textarea:DrawTextEntryText(Term.Colors['consoletext'], Color(255, 255, 255), Color(200, 200, 200)) --This is the actual textbox.
 		-- The parameters are (text color, highlight colour, cursor colour.)
 	end
@@ -106,8 +174,8 @@ function Term.Menu() --Function for drawing the menu
 	drawpath = vgui.Create("DLabel", frame) --Create the label for the prompt
 	drawpath:SetText(pathtext) --Set the text for the prompt label
 	drawpath:SetFont("terminalfont") --Set the font for the label
-	drawpath:SetTextColor(Term.Colors['consoletext']) --Set the text folour for the label
-	drawpath:SetPos(2, frame:GetTall() - 17) --Get the position for the label
+	drawpath:SetTextColor(Color(255,255,255)) --Set the text folour for the label
+	drawpath:SetPos(10, frame:GetTall() - 20) --Get the position for the label
 	drawpath:SizeToContents() --Size the label to match the length of the text
 
 	commandbox = vgui.Create("DTextEntry", frame) --Create the box for the command
@@ -159,7 +227,7 @@ function Term.Menu() --Function for drawing the menu
 		end
 
 		if command[1] == "echo" then --If the command is echo
-			if #command == 1 then Term.error("You need to pass through a string.") end -- If there is only one item in the list chuck out an error.
+			if #command == 1 then Term.alert("You need to pass through a string.") end -- If there is only one item in the list chuck out an error.
 			Term.echo(command)  --Execute the echo function is cmds/echo.lua
 								--We send through the command table and deal with it in the echo function.
 		end
@@ -192,33 +260,131 @@ function Term.Menu() --Function for drawing the menu
 	end
 
 
-	local frameclose = vgui.Create("DButton", frame) --This is our custom close button.
-	frameclose:SetSize(35, 20) --Set the size to be appropriate.
-	frameclose:SetPos(frame:GetWide() - 37, 0) --Set the pos
+	frameclose = vgui.Create("DButton", frame) --This is our custom close button.
+	frameclose:SetSize(45, 20) --Set the size to be appropriate.
+	frameclose:SetPos(frame:GetWide() - 55, 1) --Set the pos
 	frameclose:SetText("") --We don't want the text to be anything because we draw that below.
 	frameclose.DoClick = function() --On click
 		frame:Remove() --Remove the frame
+		timer.Destroy("TimerTransitionY") timer.Destroy("TimerTransitionW")
+		Term.FullSize = false
 		Term.Text = textarea:GetValue() --Set Term.Text to the textarea
 	end
 
 	function frameclose:Paint(w, h) --paint function for the button
-		--This is a little different because we want the button to have a different look if it is pressed.
-		if frameclose:IsDown() then --Check if the button is down
-			--If it is we want to do this.
-			draw.RoundedBox(0, 0, 2, w, h, Color(192, 57, 43))
-			draw.SimpleText("x", "terminaltitle", w / 2, h / 2, Color(255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-		else --If it isn't we do this
-			draw.RoundedBox(0, 0, 0, w, h, Color(239, 72, 54))
-			draw.RoundedBox(0, 0, h-2, w, 2, Color(192, 57, 43))
-			draw.SimpleText("x", "terminaltitle", w / 2, (h / 2) - 2, Color(255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+				draw.RoundedBox(0, 0, 0, w, h, Color(200, 80, 80))
+	end
+	
+	framebigger = vgui.Create("DButton", frame) --This is our custom close button.
+	framebigger:SetSize(10, 8.5) --Set the size to be appropriate.
+	framebigger:SetPos(frame:GetWide() -76, 5) --Set the pos
+	framebigger:SetText("") --We don't want the text to be anything because we draw that below.
+	framebigger.DoClick = function() --On click
+	frameposa, frameposb = 	frame:GetSize()
+		if Term.FullSize == true then 
+					dotransition()
+					Term.FullSize = false
+
+		else
+				
+					dotransition()
+					Term.FullSize = true
 		end
 	end
 
+	function framebigger:Paint(w, h) --paint function for the button
+				draw.RoundedBox(0, 0, 0, w, h, Color(62, 107, 147))
+				draw.RoundedBox(0, 1, 2, w-2, h-3,Color(84, 175, 225))
+				
+				
+				
+				
+	end
+	
+	framereduce = vgui.Create("DButton", frame) --This is our custom close button.
+	framereduce:SetSize(8, 2) --Set the size to be appropriate.
+	framereduce:SetPos(frame:GetWide() - 103, 9) --Set the pos
+	framereduce:SetText("") --We don't want the text to be anything because we draw that below.
+	framereduce.DoClick = function() --On click
+		dotreduce()
+		Term.Text = textarea:GetValue() --Set Term.Text to the textarea
+	end
+
+	function framereduce:Paint(w, h) --paint function for the button
+				draw.RoundedBox(0, 0, 0, w, h, Color(40, 40, 40))
+	end
+	
+	
 	textarea:SetCaretPos(#textarea:GetValue()) --Set the caret to the end of the text area so we get put the the bottom the the textbox.
 
 end
 --Console Commands
 concommand.Add("terminal", Term.Menu) --Add the console command.
 
+
+
+
+
+
+		
+function updatesize(posa, posb)
+				
+				tempposa, tempposb = frame:GetPos()
+
+				 if frameposa then frame:SetSize(frameposa, frameposb) end
+				if posa then frame:SetPos(posa or tempposa, posb or tempposa) end
+				textarea:SetSize(frame:GetWide() - 18, frame:GetTall() - 60) --Set the size
+				commandbox:SetSize(frame:GetWide() - (4 + drawpath:GetWide()), 20)
+				commandbox:SetPos(drawpath:GetWide() + 2, frame:GetTall() - 22)
+				frameclose:SetPos(frame:GetWide() - 55, 1)
+				framereduce:SetPos(frame:GetWide() - 103, 9)
+				drawpath:SetPos(10, frame:GetTall() - 20)
+				framebigger:SetPos(frame:GetWide() -76, 5)
+			if frameposa == ScrW() or frameposb == ScrH() then
+				timer.Destroy("TimerTransitionY")
+				timer.Destroy("TimerTransitionW")
+			end
 end
+		
+function bigmamatransition()
+		if not IsValid(frame) then 	timer.Destroy("TimerTransitionY") timer.Destroy("TimerTransitionW")  return end
+		if Term.FullSize then
+			frameposa = math.Approach(frameposa, ScrW(), 40)
+			frameposb = math.Approach(frameposb, ScrH(), 20)
+		else
+			frameposa = math.Approach(frameposa, 650, 40)
+			frameposb =	math.Approach(frameposb, 350, 20)
+		end
+		updatesize()
+	
+end
+		
+function reducemove()
+	posa, posb = frame:GetPos()
+	posa = math.Approach(posa, ScrW()/2, 40)
+	posb = math.Approach(posb, ScrH()+500, 40)
+	updatesize(posa, posb)
+end
+		
+		
+		
+function dotransition()
+	if not frameposa then frameposa, frameposb = 	frame:GetSize() end
+	timer.Create( "TimerTransitionX", 1/(ScrW()/40), ScrW()/40, bigmamatransition ) 
+	timer.Create( "TimerTransitionY",1/(ScrH()/20), ScrH()/20, bigmamatransition )
+end
+		
+function dotreduce()
+	if not frameposa then frameposa, frameposb = 	frame:GetSize() end
+	timer.Create( "TimerReduceterm", 1/(ScrW()/40), ScrW()/40, reducemove )
+	timer.Simple(1, function () timer.Destroy("TimerReduceterm")  frame:Remove() end)
+end
+		
+	
+		
+	
+end
+
+
+
 
